dbeca2df6a73cf5bc598401d41708a9a296cd9c6
Bradley <57405760+Bradley00Uni@users.noreply.github.com>
2021-06-03 22:47:57 +0100

    Version 1.2
    > Better control over Player Movement:
      - Player is locked at entrance for initial
        video
     -Once in checkout, next video starts and player cannot leave area
    > More styling
    > New C# Scripts

---
+++ Assets/Scripts/ActivateCheckout.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class ActivateCheckout : MonoBehaviour
+{
+    public GameObject checkoutVideo;
+    public GameObject checkoutWall;
+    public GameObject self;
+    VideoPlayer vp;
+
+    void Start()
+    {
+       vp = checkoutVideo.GetComponent<VideoPlayer>();
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+
+        if(Physics.SphereCast(transform.position, 1f, Vector3.up, out hit))
+        {
+            if(hit.collider.tag == "Player")
+            {
+                ActivateCheckoutScript();
+            }
+        }
+    }
+
+    void ActivateCheckoutScript()
+    {
+            vp.url = "Assets/Videos/CheckoutIntro.mp4";
+            checkoutVideo.SetActive(true);
+            checkoutWall.SetActive(true);
+            vp.Play();
+            self.SetActive(false);
+    }
+}
