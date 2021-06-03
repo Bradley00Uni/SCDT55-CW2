4cbf711f35f7d2428cbbfdb32e7dc27b7f82b31b
Bradley <57405760+Bradley00Uni@users.noreply.github.com>
2021-06-03 17:53:31 +0100

    Version 1.1
    > Added temporary videos to test instruction spawns
    > Removed unnecessary files
    >Built Shop Area
    > Resign of Shop area
    > Working checkout scanner with beep sound effect
    > If item with certain tag is scanned, trigger video
    > user starts at entrance, is instructed to go to checkout
    > Imported Shopping Models
    > Removed teleportation ability

---
+++ Assets/Scripts/CheckoutBeep.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class CheckoutBeep : MonoBehaviour
+{
+    public AudioSource beepSource;
+    public GameObject videoPlayer;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.layer == 8)
+        {
+            beepSource.Play();
+        }
+
+        if(col.gameObject.tag == "Alcohol")
+        {
+            videoPlayer.SetActive(true);
+            videoPlayer.GetComponent<VideoPlayer>().url = "Assets/Videos/booze_scanned.mp4";
+            videoPlayer.GetComponent<VideoPlayer>().Play();
+        }
+    }
+}
---
+++ Assets/Scripts/FirstVideoFinish.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class FirstVideoFinish : MonoBehaviour
+{
+    public GameObject vp;
+    public GameObject entrance;
+    public GameObject entranceblock;
+    public GameObject checkoutblock;
+    VideoPlayer player;
+    VideoPlayer entranceplayer;
+
+    void Start()
+    {
+        player = vp.GetComponent<VideoPlayer>();
+        entranceplayer = entrance.GetComponent<VideoPlayer>();
+    }
+    
+    void Update()
+    {
+        player.loopPointReached += EndVideo;
+        entranceplayer.loopPointReached += StartTraining;
+    }
+
+    void EndVideo(UnityEngine.Video.VideoPlayer vid)
+    {
+        vp.SetActive(false);
+    }
+
+    void StartTraining(UnityEngine.Video.VideoPlayer vid)
+    {
+        entrance.SetActive(false);
+        entranceblock.SetActive(false);
+        checkoutblock.SetActive(false);
+    }
+
+}
